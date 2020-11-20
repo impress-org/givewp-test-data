c62702c50c9f0d11803812afd8412239f5e3759f
alaca <ante.laca@gmail.com>
2020-11-20 11:59:28 +0100

    refactor: remove unused methods

--- src/RecurringDonations/AdminSettings/RecurringDonationsSettings.php
+++ src/RecurringDonations/AdminSettings/RecurringDonationsSettings.php
@@ -9,26 +9,6 @@ use GiveTestData\Addon\View;
  * @package GiveTestData\RecurringDonations
  */
 class RecurringDonationsSettings {
-
-	/**
-	 * Render donations options
-	 * @return void
-	 * @since 1.0.0
-	 */
-	public function renderDonationOptions() {
-		// Get active currencies
-		$statuses = give_recurring_get_subscription_statuses();
-
-		if ( ! empty( $statuses ) ) {
-			View::render(
-				'RecurringDonations.donation-options',
-				[
-					'statuses' => $statuses,
-				]
-			);
-		}
-	}
-
 	/**
 	 * Render page options
 	 * @return void
