e74e56641b5059e8197ca9b8333ba82807b1b9f4
alaca <ante.laca@gmail.com>
2020-11-24 18:15:11 +0100

    refactor: regsiter rest route; enqueue funds script

--- src/Funds/ServiceProvider.php
+++ src/Funds/ServiceProvider.php
@@ -3,11 +3,12 @@
 namespace GiveTestData\Funds;
 
 use WP_CLI;
+use Give\Helpers\Hooks;
 use GiveTestData\TestData\Helpers\SettingsPage;
 use GiveTestData\Funds\AdminSettings\FundsSettings;
 use Give\ServiceProviders\ServiceProvider as GiveServiceProvider;
 
-	/**
+/**
  * Class ServiceProvider
  * @package GiveTestData\Funds
  */
@@ -38,6 +39,22 @@ class ServiceProvider implements GiveServiceProvider {
 				return $args;
 			}
 		);
+
+		/**
+		 * Load assets
+		 */
+		add_action( 'admin_enqueue_scripts', function () {
+			wp_enqueue_script(
+				'give-test-data-script-funds',
+				GIVE_TEST_DATA_URL . 'public/js/funds.js',
+				[ 'give-test-data-script-backend' ],
+				GIVE_TEST_DATA_VERSION,
+				true
+			);
+		} );
+
+		// Register REST route
+		Hooks::addAction( 'rest_api_init', FundsRoute::class, 'registerRoute' );
 
 		// Add settings section
 		SettingsPage::addPageSection( 'give-test-data', 'funds', esc_html__( 'Generate Funds', 'give-test-data' ) );
