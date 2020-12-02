9af2b09274dc698fa47b328c392acc5b5f1ecad4
alaca <ante.laca@gmail.com>
2020-12-02 18:51:47 +0100

    refactor: add input name

--- src/RecurringDonations/resources/views/pages-options.php
+++ src/RecurringDonations/resources/views/pages-options.php
@@ -1,11 +1,11 @@
 <?php defined( 'ABSPATH' ) or exit; ?>
 
 <tr>
-	<td class="row-title give-test-data-row-width">
+    <td class="row-title">
 		<?php esc_html_e( 'Recurring Donations demonstration page', 'give-test-data' ); ?>
-		<p><?php esc_html_e( 'Pages that demonstrate Recurring Donations shortcodes/blocks', 'give-test-data' ); ?></p>
-	</td>
-	<td colspan="2">
-		<input type="checkbox"/>
-	</td>
+        <p><?php esc_html_e( 'Pages that demonstrate Recurring Donations shortcodes/blocks', 'give-test-data' ); ?></p>
+    </td>
+    <td>
+        <input name="give_test_data_recurring_demonstration_page" type="checkbox"/>
+    </td>
 </tr>
