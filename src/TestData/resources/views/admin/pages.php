8ea736c4a676caf060403dd96164540ddb1b7732
alaca <ante.laca@gmail.com>
2020-12-02 19:02:51 +0100

    refactor: simplify generation page

--- src/TestData/resources/views/admin/pages.php
+++ src/TestData/resources/views/admin/pages.php
@@ -1,35 +1,15 @@
 <?php defined( 'ABSPATH' ) or exit; ?>
 
-<h3><?php esc_html_e( 'Pages', 'give-test-data' ); ?></h3>
+<h3><?php esc_html_e( 'GiveWP demonstration page', 'give-test-data' ); ?></h3>
 
-<table class="give-test-data-table give-table">
-	<tbody>
+<p><?php esc_html_e( 'Page that demonstrate core shortcodes/blocks', 'give-test-data' ); ?></p>
 
-		<?php do_action( 'give-test-data-pages-start' ); ?>
-
-		<tr>
-			<td class="row-title">
-				<?php esc_html_e( 'GiveWP demonstration page', 'give-test-data' ); ?>
-				<p><?php esc_html_e( 'Pages that demonstrate core shortcodes/blocks', 'give-test-data' ); ?></p>
-			</td>
-			<td colspan="2">
-				<input type="checkbox" checked/>
-			</td>
-		</tr>
-
-		<?php do_action( 'give-test-data-pages-end' ); ?>
-
-	</tbody>
-</table>
-
-<?php do_action( 'give-test-data-after-pages-table' ); ?>
-
-<br />
+<br/>
 
 <div>
-	<button class="button button-primary">
-		<?php esc_html_e( 'Generate Pages', 'give-test-data' ); ?>
-	</button>
+    <button id="give-test-data-generate-page" class="button button-primary">
+		<?php esc_html_e( 'Generate Page', 'give-test-data' ); ?>
+    </button>
 </div>
 
 
